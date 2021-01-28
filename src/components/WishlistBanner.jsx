5af1c76b55c2ac226b864fbf33833c2076ea91e6
Chuck Vaughan <chuck3774@hotmail.com>
2021-01-28 13:11:06 -0700

    Update styling

--- src/components/WishlistBanner.jsx
+++ src/components/WishlistBanner.jsx
@@ -1,14 +1,7 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import { Toast } from 'react-bootstrap';
 import 'react-multi-carousel/lib/styles.css';
-
-const MaxToast = styled(Toast)`
-background-color: rgb(254, 245, 210);
-max-width: 1300px;
-text-align: center;
-border-color: rgb(253, 128, 36);
-`;
+import styled from 'styled-components';
 
 const StyledImage = styled.img`
   width: 30px;
@@ -20,14 +13,25 @@ const WishlistBanner = () => {
   const [show, setShow] = useState(true);
   return (
     <div>
-      <MaxToast onClose={() => setShow(false)} show={show} delay={3000} autohide>
+      <Toast
+        style={{
+          backgroundColor: 'rgb(254, 245, 210)',
+          maxWidth: '1300px',
+          textAlign: 'center',
+          borderColor: 'rgb(253, 128, 36)',
+        }}
+        onClose={() => setShow(false)}
+        show={show}
+        delay={3000}
+        autohide
+      >
         <Toast.Body>
           <StyledImage src="https://chuck3774bucket.s3.us-east-2.amazonaws.com/wishlist-2b84a8649efbd1ac3f97049b4a287c26.svg" alt="Heart" />
 
           ADDED TO WISHLIST
 
         </Toast.Body>
-      </MaxToast>
+      </Toast>
 
     </div>
 
