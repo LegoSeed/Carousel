a626ac230431069aaa37fa1585be8b08b9ff6e95
Chuck Vaughan <chuck3774@hotmail.com>
2021-01-28 16:30:06 -0700

    Add column

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-no-bind */
 /* eslint-disable max-len */
 import React from 'react';
+import { Col } from 'react-bootstrap';
 import ProductList from './components/ProductList';
 import './style.css';
 
@@ -31,11 +32,10 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <div className="buffer">
-          <ProductList items={this.state.recommendedProducts} wasClicked={this.state.clicked} wasClicked2={this.state.clicked2} click={this.OnAddToBagClick.bind(this)} click2={this.OnAddToWishlistClick.bind(this)} />
-        </div>
-      </div>
+      <Col class="col-12" className="buffer">
+        <ProductList items={this.state.recommendedProducts} wasClicked={this.state.clicked} wasClicked2={this.state.clicked2} click={this.OnAddToBagClick.bind(this)} click2={this.OnAddToWishlistClick.bind(this)} />
+
+      </Col>
     );
   }
 }
