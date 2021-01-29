eb6a95bc7338a237b993963ad3b79bceffc65a1a
Chuck Vaughan <chuck3774@hotmail.com>
2021-01-28 17:01:41 -0700

    stuff4

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -8,6 +8,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import Carousel from 'react-multi-carousel';
+import { Row } from 'react-bootstrap';
 import Product from './Product';
 import 'react-multi-carousel/lib/styles.css';
 import BagBanner from './BagBanner';
@@ -44,23 +45,29 @@ const responsive = {
 
 const ProductList = (props) => (
   <div>
-    {props.wasClicked ? <BagBanner /> : ''}
-    {props.wasClicked2 ? <WishlistBanner /> : ''}
-    <h1 className="title">
-      Recommended For You
-    </h1>
-    <Carousel
-      responsive={responsive}
-      customDot={<CustomDot />}
-      showDots
-      ssr
-      partialVisible
-      itemClass="image-item"
-    >
-      {
+    <Row>
+      {props.wasClicked ? <BagBanner /> : ''}
+      {props.wasClicked2 ? <WishlistBanner /> : ''}
+
+    </Row>
+    <Row>
+
+      <h1 className="title">
+        Recommended For You
+      </h1>
+      <Carousel
+        responsive={responsive}
+        customDot={<CustomDot />}
+        showDots
+        ssr
+        partialVisible
+        itemClass="image-item"
+      >
+        {
     props.items.map((itemObj, i) => <Product item={itemObj} key={i} Clicked={props.click} Clicked2={props.click2} />)
     }
-    </Carousel>
+      </Carousel>
+    </Row>
   </div>
 );
 export default ProductList;
