c9471befca024b710db5f175a2fe1f91be129e11
Ryan Evanyo <ryan.evanyo@gmail.com>
2021-02-05 10:39:04 -0700

    Seeder with Get Routes

--- server/app.js
+++ server/app.js
@@ -2,23 +2,47 @@ const compression = require('compression');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const Item = require('../database.js');
+const db = require('../dataBase/index.js');
+// const Item = require('../database.js');
 
 const app = express();
-app.use(compression());
+app.use(express.json());
 app.use(cors());
 
-const corsOptions = {
-  maxAge: 31536000,
-};
+app.use(express.static(path.join(__dirname, '..', 'public')));
+const PORT = 3001;
 
-app.get('/items', cors(corsOptions), (req, res) => {
-  Item.find({}).limit(8)
-    .then((products) => {
-      res.status(200).json(products);
-    });
+app.get('/carousel', (req, res) => {
+  console.log(req.body);
+  const values = req.body.name;
+  db.query("SELECT * FROM products WHERE name = $1", [values], (err, data) => {
+    if (err) {
+      console.log('error');
+      res.status(400);
+    } else {
+      console.log('yes');
+      res.status(200);
+      res.send(data);
+    }
+  });
 });
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.post('/carousel', (req, res) => {
+  console.log(req.body);
+  const text = 'insert into test';
+  const values = req.body.name;
+  db.query(text, values, (err, data) => {
+    if (err) {
+      res.status(400);
+    } else {
+      res.status(200);
+      res.send(data);
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('Server is listening on: ', PORT);
+});
 
 module.exports = app;
